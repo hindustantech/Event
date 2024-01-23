dd9603ceaf3e885c5f36f8dc5783185c8d6076d2
hindustantech <145006668+hindustantech@users.noreply.github.com>
2024-01-22 18:26:17 -0800

    change in det env file

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -8,10 +8,10 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
 
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRE = process.env.WEBHOOK_SECRE
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+  if (!WEBHOOK_SECRE) {
+    throw new Error('Please add WEBHOOK_SECRE from Clerk Dashboard to .env or .env.local')
   }
 
   // Get the headers
@@ -32,7 +32,7 @@ export async function POST(req: Request) {
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(WEBHOOK_SECRE);
 
   let evt: WebhookEvent
 
